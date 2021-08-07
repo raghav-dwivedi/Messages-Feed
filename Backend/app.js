84cef9419037a7389b39e555a094b43628c1091d
Raghav Dwivedi <rd9008000@gmail.com>
2021-08-07 23:39:17 +0530

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const multer = require('multer');
+
+const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, 'images');
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '-' + file.originalname);
+	},
+});
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/jpeg' ||
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+};
+
+// data format of urlencoded body parser: x-www-form-urlencoded
+app.use(express.json()); // data format: application/json
+app.use(
+	multer({
+		storage: fileStorage,
+		fileFilter: fileFilter,
+	}).single('image')
+);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.use((req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'GET, POST, PUT, PATCH, DELETE'
+	);
+	res.setHeader(
+		'Access-Control-Allow-Headers',
+		'Content-Type, Authorization'
+	);
+	next();
+});
+
+app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
+
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	const data = error.data;
+	res.status(status).json({
+		message: message,
+		data: data,
+	});
+});
+
+mongoose
+	.connect(
+		'mongodb+srv://raghav:IqVmtHF4WEcDKkOJ@cluster0.bhzjg.mongodb.net/messages?retryWrites=true&w=majority',
+		{
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		}
+	)
+	.then((result) => {
+		console.log('Connected');
+		app.listen(8080);
+	})
+	.catch((err) => console.log(err));
---
+++ Backend/routes/auth.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const router = express.Router();
+
+const User = require('../models/user');
+const authController = require('../controllers/auth');
+
+const isAuth = require('../middleware/is-auth');
+
+router.put(
+	'/signup',
+	[
+		body('email')
+			.isEmail()
+			.withMessage('Please enter a valid email address')
+			.custom((value, { req }) => {
+				return User.findOne({ email: value }).then((userDoc) => {
+					if (userDoc) {
+						return Promise.reject('Email already exists');
+					}
+				});
+			})
+			.normalizeEmail(),
+
+		body('password').trim().isLength({ min: 5 }),
+		body('name').trim().not().isEmpty(),
+	],
+	authController.signup
+);
+
+router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.patch(
+	'/status/update',
+	[body('status').trim().not().isEmpty()],
+	isAuth,
+	authController.updateUserStatus
+);
+
+module.exports = router;
